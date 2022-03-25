99b4d70d03e94fdebdfccde24da809f847157e82
aboubakarchabi <aboubakaroumarou6@gmail.com>
2022-03-25 14:01:47 +0100

    my first commi

---
+++ app.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from joblib import load
+import numpy as np
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+# load model
+model = load('pipeline1_credit1.joblib')
+
+@app.route("/predict", methods = ["POST"])
+def predict():
+
+    data = request.get_json(force=True)
+
+
+    prediction = model.predict([list(data.values())])
+
+    output = prediction[0]
+    return jsonify({"prediction": int(output)})
+
+if __name__ == "__main__":
+    app.run(debug=True)
