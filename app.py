1274a7a1c23178de689dba2c0a6f499e9383a48c
aboubakarchabi <aboubakaroumarou6@gmail.com>
2022-03-25 16:56:56 +0100

    tw

--- app.py
+++ app.py
@@ -19,4 +19,4 @@ def predict():
     return jsonify({"prediction": int(output)})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=True, port=5000)
--- test.py
+++ test.py
@@ -2,6 +2,6 @@ import requests
 
 data = {'param1':0, 'param2': 0, 'param3': 0, 'param4': 0, 'param5': 0,'param6': 0,'param7': 0,'param8': 0,'param9': 0,'param10': 0}
 
-url = 'http://127.0.0.1:5000/predict'
+url = 'https://appocproj.herokuapp.com/predict'
 r = requests.post(url,json=data)
 print(r.json())
